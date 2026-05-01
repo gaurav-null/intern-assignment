29218cd1fea03c2f442e7d1730205cac1373761e
gaurav-null <gauravrn02@gmail.com>
2026-05-01 18:52:55 +0000

    Fix client-side routing for GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,7 +6,7 @@ import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/intern-assignment">
       <AuthProvider>
         <App />
       </AuthProvider>
